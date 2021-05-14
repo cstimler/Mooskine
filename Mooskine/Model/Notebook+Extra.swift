df17349fab7a36b9b0c8212af1685f93e339477e
cstimler <charles.ces@gmail.com>
2021-05-13 21:32:45 -0700

    Beginning to assemble the core data model.

---
+++ Mooskine/Model/Notebook+Extra.swift
@@ -0,0 +1,13 @@
+//
+//  Notebook+Extra.swift
+//  Mooskine
+//
+//  Created by June2020 on 5/13/21.
+//  Copyright © 2021 Udacity. All rights reserved.
+//
+
+import Foundation
+
+extension Notebook {
+    
+}
